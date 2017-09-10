7661b436376e1e4a31a763d085df0b40f385011d
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-10 12:05:12 +0530

    To illustrate the concept of break Statement in java.

---
+++ BreakStatement1.java
@@ -0,0 +1,24 @@
+class Breakstatement
+{
+  public static void main(String[] args)
+  { 
+    int x=0;
+    switch(x)
+    {
+      case 0:
+      System.out.println(0);
+      case 1:
+      System.out.println(1);
+      case 2:
+      System.out.println(2);
+      case 3:
+      System.out.println(3);
+      break;// all statement will be executed until break if case label is matched 
+      case 4:
+      System.out.println(3);
+      default: 
+      System.out.println(3);
+    }
+  }
+}
+ 
