c8348f67c2eee6c9430af71af71c398494c8c21c
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-12 08:58:45 +0530

    To illustrate the concept of do while Statement in java.

---
+++ DoWhileStatement7.java
@@ -0,0 +1,14 @@
+class DoWhileStatement7
+{
+  public static void main(String[] args)
+  {  
+   int a=10,b=20;
+    do
+      {
+         System.out.println("hello");//hello.....
+      }while(a<b);
+
+      System.out.println("hiiiii");//reachable b'z a  b are not final variable
+   }
+}
+  
