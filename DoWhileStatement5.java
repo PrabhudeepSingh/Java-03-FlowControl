95bc2d9e1c9cd9cc11e7c600a2224499055822aa
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-12 08:57:48 +0530

    To illustrate the concept of do while Statement in java.

---
+++ DoWhileStatement5.java
@@ -0,0 +1,14 @@
+class DoWhileStatement5
+{
+  public static void main(String[] args)
+  { 
+    do
+      {
+         System.out.println("hello");//hello......
+      }while(true);
+
+      //System.out.println("hiiiii");//CE:unreachable statement
+   }
+}
+      
+    
