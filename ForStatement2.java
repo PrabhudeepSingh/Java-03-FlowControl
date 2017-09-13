66dae053cd97208eeef8f4d8822d597886840fe8
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-13 11:17:23 +0530

    To illustrate the concept of for Statement in java.

---
+++ ForStatement2.java
@@ -0,0 +1,12 @@
+public class ForStatement2
+{ 
+  public static void main(String []args)
+    {
+       int i=0;
+       for(System.out.println("hello boss ur sleeping");i<3;i++) //initialization section will be executed only once
+       {
+          System.out.println("hello boss u only sleeping");
+       }
+    }
+}
+      
