2df2f24d3fd29bb686c4a1e42cfc7ee85795ac96
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-13 11:09:43 +0530

    To illustrate the concept of do while Statement in java.

---
+++ DoWhileStatement8.java
@@ -0,0 +1,13 @@
+class DoWhileStatement8
+{
+  public static void main(String[] args)
+  {  
+   int a=10,b=20;
+    do
+      {
+         System.out.println("hello"); //hello
+      }while(a>b);
+
+      System.out.println("hi"); //hi
+   }
+}
