3c2f034056164cc2381746b982dc8a63ba42a0ca
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-13 11:16:32 +0530

    To illustrate the concept of for Statement in java.

---
+++ ForStatement1.java
@@ -0,0 +1,9 @@
+public class ForStatement1
+{ 
+  public static void main(String []args)
+    {
+       for(int i=0;true;i++)
+       System.out.println("hello");
+       //int x=10;      ->declarative statement not allowed
+     }
+}
