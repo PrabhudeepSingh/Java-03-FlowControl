31d93bf5d7881ca51d2689fa0890e22e7cc859c8
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-14 11:01:44 +0530

    To illustrate the concept of for Statement in java.

---
+++ ForStatement4.java
@@ -0,0 +1,14 @@
+public class ForStatement4
+{ 
+  public static void main(String []args)
+    {
+       for(int i=0; ;i++)
+       {
+       	  System.out.println("hello");
+       }
+       //System.out.println("hi") -> CE:unreachable statement
+     } 
+}
+
+
+      
