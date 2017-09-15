39bd14cbf623a0c85ebddaa5b0dc7318bf26e8cd
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-15 11:07:18 +0530

    To illustrate the concept of if else Statement in java.

---
+++ IfElseStatement2.java
@@ -0,0 +1,15 @@
+class IfElseStatement2
+{
+  public static void main(String[] args)
+  {
+    boolean b=true;
+    if(b=false)
+    {
+      System.out.println("hello");
+    }
+    else
+    {  
+      System.out.println("hiii");//hiii
+    }
+  }
+}
