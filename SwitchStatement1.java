d3e37175c21680e70be62b2ca35f7b7dd71a8bef
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-16 06:16:55 +0530

    To illustrate the concept of switch Statement in java.

---
+++ SwitchStatement1.java
@@ -0,0 +1,17 @@
+class SwitchStatement1
+{
+  public static void main(String[] args)
+  {
+    int x=10;
+
+    switch(x)
+    {
+    
+    } //empty switch is valid
+    
+    /*switch(x)
+    {
+      System.out.println("Hello");  -> CE: independent statement are not allowed.
+    }*/
+   }
+}
